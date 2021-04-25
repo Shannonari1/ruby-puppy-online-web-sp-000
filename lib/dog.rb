ce0ce57448110b330b088dc29059d07a2ca0842f
Shannon Young <shannon.ari1@gmail.com>
2021-04-25 13:43:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,7 +12,7 @@ class Dog
   end
 
   def self.clear_all
-    @@all = [] 
+    @@all = []
   end
 
   def self.print_all
@@ -42,7 +42,7 @@ endclass Dog
   end
 
   def self.clear_all
-    @@all = [] 
+    @@all = []
   end
 
   def self.print_all
